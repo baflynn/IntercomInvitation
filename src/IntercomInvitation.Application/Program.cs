ed9043d0e6806b97f059e3071f995dddff99266a
Brian Flynn <brian.flynn@wonga.com>
2015-09-28 12:56:44 +0100

    Adding missing exception handling

--- src/IntercomInvitation.Application/Program.cs
+++ src/IntercomInvitation.Application/Program.cs
@@ -7,7 +7,15 @@ namespace IntercomInvitation.Application
     {
         private static void Main(string[] args)
         {
-            App.GenerateInviations(Path.GetFullPath(args[0]));
+            try
+            {
+                App.GenerateInviations(Path.GetFullPath(args[0]));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was a problem processing the file");
+                Console.WriteLine("Exception is {0}", e.ToString());
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue...");
